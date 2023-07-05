4030b5c747d9e106d8a83d9410a228f6dbf5964a
eszypulski <111613262+eszypulski@users.noreply.github.com>
2023-07-04 22:34:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('loaded');
+
+let map = L.map('map').setView([37.090240, -95.712891], 4.5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
